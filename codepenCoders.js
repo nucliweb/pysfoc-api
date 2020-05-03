55a2248ae910e6010e9ee1ac1f681ca9eec74d62
Joan León | @nucliweb <joan.leon@gmail.com>
2020-05-03 18:13:06 +0200

    Scraping nucliweb Codepen account

---
+++ codepenCoders.js
@@ -0,0 +1,1 @@
+[{"name":"Joan Leon","avatar":"https://s.cdpn.io/profiles/user/14381/512.jpg?1574875780","followers":"234","bio":"Frontend @ Adevinta Spain | @GoogleDevExpert in webtech | #MDE @cloudinary | ❤️ CSS & #webperf","isPro":false}]
---
+++ index.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const iPhone = puppeteer.devices['iPhone 8']
+const BASE_URL = 'https://codepen.io/'
+
+const scrap = async ({user}) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.emulate(iPhone)
+
+  let codepenCoders = []
+
+  const getPageData = async () => {
+    await page.goto(`${BASE_URL}${user}`)
+    await page.screenshot({
+      path: `screenshots/${user}.png`,
+      fullPage: true
+    })
+    const data = await page.evaluate(() => {
+      const name = document.querySelector('#profile-name-header').textContent
+      const avatar = document.querySelector('#profile-image').src
+      const followers = document.querySelector('#followers-count').textContent
+      const bio = document.querySelector('#profile-bio').textContent
+      const isPro = !!document.querySelector('#profile-badge-pro')
+
+      return {
+        name,
+        avatar,
+        followers,
+        bio,
+        isPro
+      }
+    })
+    codepenCoders = [...codepenCoders, data]
+    fs.writeFile('codepenCoders.js', JSON.stringify(codepenCoders), () => {
+      console.log('codepenCoders writed 😊')
+    })
+    await browser.close()
+  }
+
+  getPageData()
+}
+
+scrap({user: 'nucliweb'})
